ca45d60d84df34c21691246a521b00d0746e7668
Dalton Stevens <d.72.stevens@gmail.com>
2024-02-09 18:29:43 -0600

    code wars filter array challenge

---
+++ filterArray.js
@@ -0,0 +1,7 @@
+const filterArray = (arr) => {
+  return arr.filter((x) => typeof x === "number");
+};
+
+console.log(filterArray([1, 2, "a", "b"]));
+console.log(filterArray([1, "a", "b", 0, 15]));
+console.log(filterArray([1, 2, "aasf", "1", "123", 123]));
